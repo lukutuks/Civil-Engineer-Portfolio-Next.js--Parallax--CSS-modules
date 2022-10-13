e8317ac9cb0bfa156bbcecbcac2bd3cab59f3ce3
Lukas <lukas.krylovas@gmail.com>
2022-10-13 14:54:20 -0700

    Responsive Header
    Working on responsive header

--- components/Header.js
+++ components/Header.js
@@ -1,36 +1,31 @@
 import React from 'react'
 import styles from '../styles/Header.module.css'
 import { AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import toggleMenuOpen from '../utils/toggleMenuOpen';
+
 const Header = () => {
   return (
-    <div className={styles.container}>
-        <div className={styles.logo}>
-            <a> 
-                <span>Jonas Šimutis</span>
-            </a>
-        </div>
+    <nav className={styles.navigation}>
+      <div className={styles.navbarOverlay}
+      onClick={toggleMenuOpen}></div>
+      <button 
+    type="button"
+    className={styles.hamburger}
+    onClick={toggleMenuOpen}>
+    
+    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg>
+    </button>
+    
+    <a href="/" className={styles.logo}>
+      JONAS ŠIMUTIS
+    </a>
+   <nav className={styles.navMenu}>
+    <button type='button' className={styles.navButton}>About</button>
+    <button type='button' className={styles.navButton}>Projects</button>
+    <button type='button' className={styles.navButton}>Contact</button>
+   </nav>
+  </nav>
 
-        <div className={styles.navContainer}>
-        <a href="/about" class="active">
-          About
-        </a>
-        <a href="/engineering" class="active">
-          Engineering
-        </a> <a href="/projects" class="active">
-          Projects
-        </a> <a href="/contact" class="active">
-          Contact
-        </a>
-        </div>
-        <div className={styles.socialIcons}>
-        <a className={styles.socialIcon} href="https://www.linkedin.com/in/jonas-simutis" target="_blank">
-          <AiFillLinkedin size="3rem" />
-        </a>
-        <a className={styles.socialIcon} href="https://www.instagram.com/lukkutis/" target="_blank">
-          <AiFillInstagram size="3rem"/>
-        </a>
-        </div>
-    </div>
   )
 }
 
---
+++ utils/toggleMenuOpen.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const toggleMenuOpen = () =>{
+    document.body.classList.toggle("open");
+}
+
+export default toggleMenuOpen
